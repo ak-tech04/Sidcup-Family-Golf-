096eb102e2a4e5c7398117a1d10a9684cb314a24
aditya <ak.dev2004@gmail.com>
2025-07-26 21:26:15 +0530

    created landing page

--- script.js
+++ script.js
@@ -34,3 +34,75 @@ gsap.to("#main", {
     scrub: 2,
   },
 });
+
+gsap.from("#about-us", {
+  y: 150,
+  // stagger:5,
+  // x: 100,
+  scrollTrigger: {
+    trigger: "#about-us",
+    scroll: "body",
+    start: "top 80%",
+    end: "bottom 40%",
+    scrub: 2,
+    // markers: true,
+  },
+});
+gsap.from("#card-container", {
+  y: 100,
+  x: 500,
+  opacity: 0,
+  scrollTrigger: {
+    trigger: "#card-container",
+    scroller: "body",
+    start: "top 80%",
+    end: "50% 50%",
+    scrub: 2,
+    // markers:  true,
+  },
+});
+let colonTL = gsap.timeline();
+
+colonTL.from("#colon-left", {
+  y: -100,
+  x: -50,
+  opacity: 0,
+  scrollTrigger: {
+    trigger: "#page3",
+    scroller: "body",
+    start: "20% 80%",
+    end: "40% 40%",
+    scrub: 2,
+  },
+});
+colonTL.from("#colon-right", {
+  y: 80,
+  x: 50,
+  opacity: 0,
+
+  scrollTrigger: {
+    trigger: "#page3",
+    scroller: "body",
+    start: "20% 80%",
+    end: "40% 40%",
+    scrub: 2,
+  },
+});
+gsap.from("#page4>h2",{
+  x: 100,
+  opacity: 0,
+  scrollTrigger: {
+    trigger:"#page4",
+    scroller:"body",
+    start: "top 80%",
+    end: 'center 50%',
+    
+    // markers: true,
+    scrub: 2,
+  }
+})
+
+// gsap.from('#page4 img', {
+//   scale: 1,
+  
+// })
